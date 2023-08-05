a1fb03f37485fb18a0446b3ea28bfdcd82341b5e
awal99 <awalsulemana3@gmail.com>
2023-08-05 10:07:50 +0000

    fix audit recommendations

--- authenticator.sol
+++ authenticator.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-///@audit - Use a fixed pragma version to prevent breaking changes, preferrably a version right before the latest. In this case, 0.8.20 would be a good choice
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.2;
 
 contract UserManagement {
     enum UserRole {
@@ -14,10 +13,11 @@ contract UserManagement {
      * @dev - Not a great idea to store strings on-chain. Strings are expensive to store and manipulate on-chain and also expose users information to the public.
      * Users emails and usernames are stored in the clear on-chain, which is a security risk, best to use a much secure method of storing this information.
      * @audit - Consider using a bytes32 instead of a string to store the username and email. This will save on gas costs and prevent users information from being exposed.
+     * @dev DONE
      */
     struct User {
-        string username;
-        string email;
+        bytes32 username;
+        bytes32 email;
         bytes32 encryptedPassword;
         UserRole role;
     }
@@ -26,15 +26,16 @@ contract UserManagement {
      * @dev - Would not recommend using strings as keys in a mapping.
      * The use of strings is not recommended in general, as they are expensive to store and manipulate on-chain.
      * It is cheaper to use bytes32 or uint256 as keys. Better still the user's address can be used as the key.
+     * @dev DONE
      */
     mapping(address => User) public users;
-    mapping(string => bool) public usernameExists;
-    mapping(string => bool) public emailExists;
+    mapping(address => bool) public usernameExists;
+    mapping(bytes32 => bool) public emailExists;
 
     event UserSignedUp(
         address indexed userAddress,
-        string username,
-        string email,
+        bytes32 username,
+        bytes32 email,
         UserRole role
     );
     event UserRoleUpdated(address indexed userAddress, UserRole role);
@@ -49,7 +50,7 @@ contract UserManagement {
 
     modifier userExists() {
         require(
-            bytes(users[msg.sender].username).length > 0,
+            bytes32(users[msg.sender].username).length > 0,
             "User does not exist"
         );
         _;
@@ -59,42 +60,45 @@ contract UserManagement {
      * @dev it is worth noting, naming of variables and functions differ based on the visibility.
      * For example, using underscores for private and internal variables/functinos and camelCase for public variables unless otherwise.
      * This is not a requirement but a good practice to follow.
+     * @dev DONE
      */
     function signUp(
-        string memory _username,
-        string memory _email,
-        bytes32 _encryptedPassword
+        bytes32 username,
+        bytes32 email,
+        bytes32 encryptedPassword
     ) external {
-        require(!usernameExists[_username], "Username already taken");
-        require(!emailExists[_email], "Email already registered");
+        require(!usernameExists[msg.sender], "Username already taken");
+        require(!emailExists[email], "Email already registered");
 
         User memory newUser = User({
-            username: _username,
-            email: _email,
-            encryptedPassword: _encryptedPassword,
+            username: username,
+            email: email,
+            encryptedPassword: encryptedPassword,
             role: UserRole.User
         });
 
         ///@audit Users mapping will be overwritten if the same address is used to sign up multiple times with different usernames and emails.
         ///@audit - Consider using the user's address as the key instead of the username, this will prevent users from signing up multiple times with the same address
         ///@dev - Alternatively, the address can be tied to the user's username and email in the frontend thereby only requiring a hashed password to be stored on-chain.
+        ///@dev DONE
 
         users[msg.sender] = newUser;
-        usernameExists[_username] = true;
-        emailExists[_email] = true;
+        usernameExists[msg.sender] = true;
+        emailExists[email] = true;
 
-        emit UserSignedUp(msg.sender, _username, _email, UserRole.User);
+        emit UserSignedUp(msg.sender, username, email, UserRole.User);
     }
 
     function login(
-        string memory _email,
+        bytes32 _email,
         bytes32 _encryptedPassword
-    ) external view returns (string memory) {
+    ) external view returns (bytes32) {
         require(emailExists[_email], "Email not registered");
 
         ///@audit gas optimization - Load the user into memory instead of storage since we are not modifying the user.
         ///@dev - This will save on gas costs as it is cheaper to work with memory than storage.
-        User storage user = users[msg.sender];
+        ///@dev DONE
+        User memory user = users[msg.sender];
 
         ///@dev careful using encodePacked, it can lead to collisions. Be sure to use a salt or nonce to prevent possible collisions.
         require(
@@ -124,7 +128,8 @@ contract UserManagement {
 
     /// @dev - This getter function should accept an address as an argument and return the user's role instead of the msg.sender's role.
     /// @dev - Ensure to pass that address to the userExists modifier to ensure the user exists.
-    function getUserRole() external view userExists returns (UserRole) {
-        return users[msg.sender].role;
+    /// @dev DONE
+    function getUserRole(address userAddress) external view userExists returns (UserRole) {
+        return users[userAddress].role;
     }
 }
